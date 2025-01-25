7533427295482e72c948b1dc38a7a4f17f439d61
Patrick <patricknbruce@gmail.com>
2025-01-25 00:03:07 -0800

    removed lambda part of update stats lambda
    the work load is better executed by ECS becuase we want to spread our requests out overtime and use state to only update on changes.

---
+++ update-stats/target/debug/deps/update_stats-fb1f0edd685fe4c6.d
@@ -0,0 +1,5 @@
+/Users/patrick/workplace/pokemon-showdown-user-stats/update-stats/target/debug/deps/update_stats-fb1f0edd685fe4c6: src/main.rs
+
+/Users/patrick/workplace/pokemon-showdown-user-stats/update-stats/target/debug/deps/update_stats-fb1f0edd685fe4c6.d: src/main.rs
+
+src/main.rs:
---
+++ update-stats/target/debug/update-stats.d
@@ -0,0 +1,1 @@
+/Users/patrick/workplace/pokemon-showdown-user-stats/update-stats/target/debug/update-stats: /Users/patrick/workplace/pokemon-showdown-user-stats/update-stats/src/main.rs
