866312649e2da09c086f2e19b30f83f06d0aa1c6
Patrick <patricknbruce@gmail.com>
2025-02-01 03:50:28 -0800

    enable update-stats container to run on ECS

--- update-stats/src/main.rs
+++ update-stats/src/main.rs
@@ -1,24 +1,9 @@
-// use aws_config::BehaviorVersion;
-// use aws_sdk_dynamodb as dynamodb;
-// use std::time::Duration;
-// use tokio::time::sleep;
-use std::env;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    // let config = aws_config::defaults(BehaviorVersion::latest()).load().await;
-    // let ddb = dynamodb::Client::new(&config);
-    let user_stats_table_env_var = "USER_STATS_TABLE";
-    let user_stats_table = match env::var(user_stats_table_env_var) {
-        Ok(table) => table,
-        Err(_) => {
-            print!("Failed to get {user_stats_table_env_var} from environment");
-            return;
-        }
-    };
     loop {
-        println!("table name: {user_stats_table}");
+        println!("Hello, world!");
         thread::sleep(Duration::from_secs(3600));
     }
 }
