639d6a2ee8d7c5fe8988e8abdaa6a75b67bd3b31
Patrick <patricknbruce@gmail.com>
2025-03-05 00:00:02 -0800

    add CF dist

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 const cert_stack = new CertificateStack(app, 'CertificateStack', {
   env: { account: '147997160234', region: 'us-east-1' },
 });
-cont certificateArn = "arn:aws:acm:us-east-1:147997160234:certificate/1d921caf-d085-4cf9-8e73-bf4d631963f6";
+const certificateArn = "arn:aws:acm:us-east-1:147997160234:certificate/1d921caf-d085-4cf9-8e73-bf4d631963f6";
 
 new InfrastructureStack(app, 'InfrastructureStack', { certificateArn, 
   env: { account: '147997160234', region: 'us-west-2' },
