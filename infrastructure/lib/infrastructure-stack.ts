03edc345b9d03767543cf19d64eec3452aafe996
Patrick <patricknbruce@gmail.com>
2025-03-07 20:23:07 -0800

    deploy all stacks with make

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -26,8 +26,8 @@ export class InfrastructureStack extends cdk.Stack {
     const addUserLambda = new lambda.Function(this, "AddUser", {
       runtime: lambda.Runtime.PROVIDED_AL2023,
       handler: "does.not.matter",
-      code: lambda.Code.fromAsset(path.join(__dirname, "..", "..", 
-          "add-user-lambda/target/lambda/add-user-lambda")),
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "..",
+        "add-user-lambda/target/lambda/add-user-lambda")),
       logRetention: logs.RetentionDays.ONE_WEEK
     });
 
@@ -92,8 +92,8 @@ export class InfrastructureStack extends cdk.Stack {
       vpc: updateStatsVpc,
     });
 
-    const updateStatsDockerImage 
-        = ecs.ContainerImage.fromAsset(path.join(__dirname, '../../update-stats'));
+    const updateStatsDockerImage
+      = ecs.ContainerImage.fromAsset(path.join(__dirname, '../../update-stats'));
 
     const updateStatsTaskDefinition = new ecs.FargateTaskDefinition(this, 'UpdateStatsTaskDef', {
       memoryLimitMiB: 512,
@@ -123,8 +123,8 @@ export class InfrastructureStack extends cdk.Stack {
     const getUserLambda = new lambda.Function(this, "GetUser", {
       runtime: lambda.Runtime.PROVIDED_AL2023,
       handler: "does.not.matter",
-      code: lambda.Code.fromAsset(path.join(__dirname, "..", "..", 
-          "get-user-lambda/target/lambda/get-user-lambda")),
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "..",
+        "get-user-lambda/target/lambda/get-user-lambda")),
       logRetention: logs.RetentionDays.ONE_WEEK
     });
 
@@ -149,72 +149,70 @@ export class InfrastructureStack extends cdk.Stack {
       'SiteCertificate',
       props.certificateArn
     );
-        // Create a custom cache policy with a TTL of 1 minute (60 seconds)
-        const cachePolicy = new cloudfront.CachePolicy(this, 'OneMinuteCachePolicy', {
-          cachePolicyName: 'OneMinuteCachePolicy',
-          defaultTtl: cdk.Duration.seconds(60),
-          minTtl: cdk.Duration.seconds(60),
-          maxTtl: cdk.Duration.seconds(60),
-          cookieBehavior: cloudfront.CacheCookieBehavior.none(),
-          headerBehavior: cloudfront.CacheHeaderBehavior.none(),
-          queryStringBehavior: cloudfront.CacheQueryStringBehavior.none(),
-        });
-    
-      // Create a CloudFront distribution that fronts the API Gateway endpoint.
-      // The origin points to the API Gateway's regional endpoint. The originPath includes the stage name ('prod').
-      const distribution = new cloudfront.Distribution(this, 'ApiDistribution', {
-        domainNames: ['pokemonshowdownuserstats.com'],
-        certificate: certificate,
-        defaultBehavior: {
-          origin: new origins.HttpOrigin(
-            `${userStatsApi.apiId}.execute-api.${this.region}.amazonaws.com`,
-            {
-              originPath: `/${userStatsApiStage.stageName}`,
-              protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
-            }
-          ),
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-          cachePolicy: cachePolicy,
-          originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
+
+    const cachePolicy = new cloudfront.CachePolicy(this, 'OneMinuteCachePolicy', {
+      cachePolicyName: 'OneMinuteCachePolicy',
+      defaultTtl: cdk.Duration.seconds(60),
+      minTtl: cdk.Duration.seconds(60),
+      maxTtl: cdk.Duration.seconds(60),
+      cookieBehavior: cloudfront.CacheCookieBehavior.none(),
+      headerBehavior: cloudfront.CacheHeaderBehavior.none(),
+      queryStringBehavior: cloudfront.CacheQueryStringBehavior.none(),
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'ApiDistribution', {
+      domainNames: ['pokemonshowdownuserstats.com'],
+      certificate: certificate,
+      defaultBehavior: {
+        origin: new origins.HttpOrigin(
+          `${userStatsApi.apiId}.execute-api.${this.region}.amazonaws.com`,
+          {
+            originPath: `/${userStatsApiStage.stageName}`,
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
+          }
+        ),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cachePolicy,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+      },
+    });
+
+
+    const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: 'pokemonshowdownuserstats.com',
+    });
+
+    new route53.ARecord(this, 'AliasRecord', {
+      zone: hostedZone,
+      recordName: 'pokemonshowdownuserstats.com',
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+
+    new budgets.CfnBudget(this, 'FreeTierBudget', {
+      budget: {
+        budgetType: 'COST',
+        timeUnit: 'MONTHLY',
+        budgetLimit: {
+          amount: 1,
+          unit: 'USD'
+        }
+      },
+      notificationsWithSubscribers: [{
+        notification: {
+          comparisonOperator: 'GREATER_THAN',
+          threshold: 0.01,
+          notificationType: 'ACTUAL'
         },
-      });
+        subscribers: [{
+          subscriptionType: 'EMAIL',
+          address: 'patricknbruce@gmail.com'
+        }]
+      }]
+    });
 
-    // const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
-    //   domainName: 'pokemonshowdownuserstats.com',
-    // });
-
-    // // Create an alias record in Route 53 to point your domain to the CloudFront distribution
-    // new route53.ARecord(this, 'AliasRecord', {
-    //   zone: hostedZone,
-    //   recordName: 'pokemonshowdownuserstats.com',
-    //   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
-    // });
-
-    // new budgets.CfnBudget(this, 'FreeTierBudget', {
-    //   budget: {
-    //     budgetType: 'COST',
-    //     timeUnit: 'MONTHLY',
-    //     budgetLimit: {
-    //       amount: 1,
-    //       unit: 'USD'
-    //     }
-    //   },
-    //   notificationsWithSubscribers: [{
-    //     notification: {
-    //       comparisonOperator: 'GREATER_THAN',
-    //       threshold: 0.01,
-    //       notificationType: 'ACTUAL'
-    //     },
-    //     subscribers: [{
-    //       subscriptionType: 'EMAIL',
-    //       address: 'patricknbruce@gmail.com'
-    //     }]
-    //   }]
-    // });
-
-    // new cdk.CfnOutput(this, 'UserStatsApiUrl', {
-    //   value: userStatsApi.apiEndpoint,
-    //   description: 'The endpoint URL of the UserStatsAp HTTP API',
-    // });
+    new cdk.CfnOutput(this, 'UserStatsApiUrl', {
+      value: userStatsApi.apiEndpoint,
+      description: 'The endpoint URL of the UserStatsAp HTTP API',
+    });
   }
 }
--- makefile
+++ makefile
@@ -4,7 +4,7 @@ build-and-deploy:
 	cd ./add-user-lambda && cargo lambda build --release --features reqwest/native-tls-vendored
 	cd ./get-user-lambda && cargo lambda build --release
 	# aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 147997160234.dkr.ecr.us-west-2.amazonaws.com
-	cd ./infrastructure && cdk synth && cdk deploy
+	cd ./infrastructure && cdk synth && cdk deploy --all
 
 watch: 
 	cd ./add-user-lambda && cargo lambda watch
