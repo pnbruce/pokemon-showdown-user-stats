8fb9fb86d8a3160d098b261eff5ea8ad015339d4
Patrick <patricknbruce@gmail.com>
2025-04-03 08:51:39 -0700

    add sentery

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ReactGA from "react-ga4";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+  dsn: "https://af4c730147c3b21166b34acf8c1a0e0c@o4509090085994496.ingest.us.sentry.io/4509090097856512",
+});
 ReactGA.initialize("G-Y5KWT1RGN0");
 
 createRoot(document.getElementById('root')!).render(
