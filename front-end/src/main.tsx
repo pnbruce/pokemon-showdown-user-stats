11ca8cd1134b18d887f21f8b9a57a08ba8913c7a
Patrick <patricknbruce@gmail.com>
2025-04-08 23:16:19 -0700

    add tracing to sentry to add stack trace

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -7,11 +7,15 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://af4c730147c3b21166b34acf8c1a0e0c@o4509090085994496.ingest.us.sentry.io/4509090097856512",
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
 });
 ReactGA.initialize("G-Y5KWT1RGN0");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>An error occurred!</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </StrictMode>,
 )
